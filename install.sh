70dd06f101aab503514829c6107234e63e3ba564
Mike Boorman <mike@learningtogeek.com>
2022-12-11 16:32:33 -0500

    tweak

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | sudo 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 18.3.0
+# nvm install 18.3.0
 
 
 cd /opt
