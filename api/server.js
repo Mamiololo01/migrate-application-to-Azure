03b8e0a1f1eb3d02e2be00903859afbe9f70ded9
learningtogeek <45207179+learningtogeek@users.noreply.github.com>
2022-12-18 16:48:26 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "plantshop",
+    database: "treefarm",
 });
 
 app.get('/', (req, res, next)=> {
